735316f580743735664a2dac5a75a65b1b713893
naveenganapathi <naveenganapathi@gmail.com>
2013-10-03 22:45:39 -0500

    Message class

---
+++ ThreePhaseCommit/src/com/tpc/util/Message.java
@@ -0,0 +1,18 @@
+package com.tpc.util;
+
+public class Message {
+	private int processId;
+	public int getProcessId() {
+		return processId;
+	}
+	public void setProcessId(int processId) {
+		this.processId = processId;
+	}
+	private String message;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
