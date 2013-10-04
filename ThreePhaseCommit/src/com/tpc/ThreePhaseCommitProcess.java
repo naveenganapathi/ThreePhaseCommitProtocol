72f61c7fd57d4bef2d5ab3ad595ebefa7dfa53bd
naveenganapathi <naveenganapathi@gmail.com>
2013-10-04 00:39:20 -0500

    Participant

--- ThreePhaseCommit/src/com/tpc/ThreePhaseCommitProcess.java
+++ ThreePhaseCommit/src/com/tpc/ThreePhaseCommitProcess.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Map;
 
 import com.tpc.util.Config;
+import com.tpc.util.Message;
 import com.tpc.util.NetController;
+import com.tpc.util.ThreePhaseCommitUtility;
 
 /***
  * 
@@ -28,7 +30,34 @@ public class ThreePhaseCommitProcess {
 			if (processId == coordinatorId) {
 				
 			} else {  // current process is a participant.
-				
+				List<String> receivedMsgs;
+				while((receivedMsgs = netController.getReceivedMsgs()).isEmpty());
+				for(String message : receivedMsgs) {
+					Message msg = ThreePhaseCommitUtility.deserializeMessage(message);
+					if(msg.getProcessId() == coordinatorId && msg.getMessage().equals("STATE_REQ")){
+						Message voteYes = new Message();
+						voteYes.setProcessId(processId);
+						voteYes.setMessage("YES");
+						netController.sendMsg(processId, ThreePhaseCommitUtility.serializeMessage(voteYes));						
+					}
+				}
+				while((receivedMsgs = netController.getReceivedMsgs()).isEmpty());
+				for(String message : receivedMsgs) {
+					Message msg = ThreePhaseCommitUtility.deserializeMessage(message);
+					if(msg.getProcessId() == coordinatorId && msg.getMessage().equals("ABORT")){
+						ThreePhaseCommitUtility.writeMessageToDTLog(processId, "ABORT");
+					}
+					if(msg.getProcessId() == coordinatorId && msg.getMessage().equals("COMMIT")){
+						ThreePhaseCommitUtility.writeMessageToDTLog(processId, "COMMIT");
+					}
+					if(msg.getProcessId() == coordinatorId && msg.getMessage().equals("PRE_COMMIT")){
+						Message ack = new Message();
+						ack.setProcessId(processId);
+						ack.setMessage("ACK");
+						netController.sendMsg(processId, ThreePhaseCommitUtility.serializeMessage(ack));
+						ThreePhaseCommitUtility.writeMessageToDTLog(processId, "COMMIT");
+					}
+				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
