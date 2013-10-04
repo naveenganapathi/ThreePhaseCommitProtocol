dfbfbbe866c1e87c6c9140851807e3aafcc2ad2c
vigneshalaguvel <vignesh.vicky3007@gmail.com>
2013-10-04 00:01:47 -0500

    basic co-ord participant block

--- ThreePhaseCommit/src/com/tpc/ThreePhaseCommitProcess.java
+++ ThreePhaseCommit/src/com/tpc/ThreePhaseCommitProcess.java
@@ -1,5 +1,37 @@
 package com.tpc;
 
+import java.util.List;
+import java.util.Map;
+
+import com.tpc.util.Config;
+import com.tpc.util.NetController;
+
+/***
+ * 
+ * @author naveen,vignesh
+ * 
+ */
 public class ThreePhaseCommitProcess {
 
+	public static void main(String args[]) {
+		if (args.length < 2) {
+			throw new IllegalArgumentException("please provide intial co-ordinator id and current process id");
+		}
+		try {
+			NetController netController = new NetController(new Config("config_"+args[1]+".txt"));
+			List<Integer> activeProcesses;
+			Map<String,String> playList;
+			int processId = Integer.parseInt(args[0]);
+			int coordinatorId = Integer.parseInt(args[1]);
+			
+			//current process is the initial co-ordinator
+			if (processId == coordinatorId) {
+				
+			} else {  // current process is a participant.
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
