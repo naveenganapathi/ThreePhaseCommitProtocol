816def1e2f1c7433d2a695368df36dc746acb458
vigneshalaguvel <vignesh.vicky3007@gmail.com>
2013-10-05 12:08:56 -0500

    co-ord code change

--- ThreePhaseCommit/src/com/tpc/ThreePhaseCommitProcess.java
+++ ThreePhaseCommit/src/com/tpc/ThreePhaseCommitProcess.java
@@ -11,6 +11,7 @@ import com.tpc.util.LogRecord;
 import com.tpc.util.Message;
 import com.tpc.util.NetController;
 import com.tpc.util.ThreePhaseCommitUtility;
+import com.tpc.util.Timer;
 
 /***
  * 
@@ -27,6 +28,7 @@ public class ThreePhaseCommitProcess {
 	public static final String NO = "NO";
 	private static final Logger log = Logger.getLogger("ThreePhaseCommitProcess");
 	private static final int NUM_PARTICIPANTS = 2;
+	public static final int TIME_OUT_IN_SECONDS = 10;
 	public static int currTrans  = 0;
 	public static void main(String args[]) {
 
@@ -40,6 +42,7 @@ public class ThreePhaseCommitProcess {
 			Map<String,String> playList;
 			int processId = Integer.parseInt(args[1]);
 			int coordinatorId = Integer.parseInt(args[0]);
+			String myVote = YES;
 
 			activeProcesses.add(0);
 			activeProcesses.add(1);
@@ -51,10 +54,11 @@ public class ThreePhaseCommitProcess {
 					Thread.sleep(5000);
 					currTrans++;
 					Message message = new Message(processId,currTrans,VOTE_REQ);
+					ThreePhaseCommitUtility.writeMessageToDTLog(processId, new LogRecord(currTrans, "start-3PC"));
 					List<Integer> participants = new ArrayList<Integer>(activeProcesses);
 					System.out.println("starting new transaction!");
 					participants.remove(processId);
-					// Send STATE REQ to all participants
+					// Send VOTE REQ to all participants
 					for(Integer participant : participants) {
 						boolean sendStatus = netController.sendMsg(participant, ThreePhaseCommitUtility.serializeMessage(message));
 						if(!sendStatus) {
@@ -62,53 +66,78 @@ public class ThreePhaseCommitProcess {
 						}
 					}
 
+					//Get VOTE responses from participants.
+					Timer timer = new Timer();
+					timer.start();
 					List<String> reportMessages = new ArrayList<String>();
-					while (reportMessages.size() != NUM_PARTICIPANTS) {
+					while (reportMessages.size() != NUM_PARTICIPANTS || timer.hasTimedOut()) {
 						reportMessages.addAll(netController.getReceivedMsgs());						
 					}
 
+					// some process timed out. aborting
+					if (reportMessages.size() != NUM_PARTICIPANTS) {
+						ThreePhaseCommitUtility.writeMessageToDTLog(processId, new LogRecord(currTrans, ABORT));
+						for (String rawMessage : reportMessages) {
+							Message msg = ThreePhaseCommitUtility.deserializeMessage(rawMessage);
+							if(msg.getMessage().equals(YES)) {
+								netController.sendMsg(msg.getProcessId(), ThreePhaseCommitUtility.serializeMessage(new Message(processId, currTrans, ABORT)));
+							}
+						}
+					}
+					
+					//abort if some process sends no.
+					boolean abortFlag = false;
+					List<Message> forMessages = new ArrayList<Message>();
+					List<Message> againstMessages = new ArrayList<Message>();
 					for (String reportMessage : reportMessages) {
 						Message msg = ThreePhaseCommitUtility.deserializeMessage(reportMessage);
 						if (msg.getMessage().equals(ABORT)) {							
-							//write ABORT to log if not already written
-							LogRecord curRecord = new LogRecord(msg.getTransactionId(), ABORT);
-							if (!ThreePhaseCommitUtility.fetchMessageFromDTLog(processId).equals(curRecord)) {
-								ThreePhaseCommitUtility.writeMessageToDTLog(processId, new LogRecord(msg.getTransactionId(),ABORT));
-							}
+							abortFlag = true;							
+						} else if ( msg.getMessage().equals(COMMIT)) {
+							forMessages.add(msg);
+						}
+					}
+
+					// some processes have decided to abort. ABORT
+					if (abortFlag || !myVote.equals(YES)) {
+						ThreePhaseCommitUtility.writeMessageToDTLog(processId, new LogRecord(currTrans,ABORT));
+						for (Message msg : forMessages) {							
 							Message abortMessage = new Message(processId,currTrans,ABORT);
-							for (Integer parts : participants) {								
-								netController.sendMsg(parts, ThreePhaseCommitUtility.serializeMessage(abortMessage));
+							netController.sendMsg(msg.getProcessId(), ThreePhaseCommitUtility.serializeMessage(abortMessage));
+						}						
+					}					
+					
+					// all processes have sent yes! COMMIT away
+					if (!abortFlag && myVote.equals(YES)) {
+						Message preCommitMessage = new Message(processId,currTrans,PRE_COMMIT);
+						for(Integer parts : participants) {
+							netController.sendMsg(parts, ThreePhaseCommitUtility.serializeMessage(preCommitMessage));
+						}
+
+						List<String> ackMessages = new ArrayList<String>();
+						Timer ackTimer = new Timer();
+						while (ackMessages.size() != NUM_PARTICIPANTS || ackTimer.hasTimedOut() ) {
+							ackMessages.addAll(netController.getReceivedMsgs());						
+						}
+
+						//just a simple check. Ideally should never happen.
+						for (String ackMessage : ackMessages) {
+							Message ackMsg = ThreePhaseCommitUtility.deserializeMessage(ackMessage);
+							if (!ackMsg.getMessage().equals("ACK")) {
+								throw new IllegalStateException("din receive ack");
 							}
 						}
-					}
 
-					//TODO assuming that all participants sent yes
-					Message preCommitMessage = new Message(processId,currTrans,PRE_COMMIT);
-					for(Integer parts : participants) {
-						netController.sendMsg(parts, ThreePhaseCommitUtility.serializeMessage(preCommitMessage));
-					}
+						//write commit to DT log
+						ThreePhaseCommitUtility.writeMessageToDTLog(processId, new LogRecord(currTrans,COMMIT));
 
-					List<String> ackMessages = new ArrayList<String>();
-					while (ackMessages.size() != NUM_PARTICIPANTS) {
-						ackMessages.addAll(netController.getReceivedMsgs());						
-					}
 
-					for (String ackMessage : ackMessages) {
-						Message ackMsg = ThreePhaseCommitUtility.deserializeMessage(ackMessage);
-						if (!ackMsg.getMessage().equals("ACK")) {
-							throw new IllegalStateException("din receive ack");
+						//send COMMIT to everyone
+						Message commitMessage = new Message(processId,currTrans,COMMIT);
+						for(Integer parts : participants) {
+							netController.sendMsg(parts, ThreePhaseCommitUtility.serializeMessage(commitMessage));
 						}
-					}
-
-					//write commit to DT log
-					ThreePhaseCommitUtility.writeMessageToDTLog(processId, new LogRecord(currTrans,COMMIT));
-
-
-					//send COMMIT to everyone
-					Message commitMessage = new Message(processId,currTrans,COMMIT);
-					for(Integer parts : participants) {
-						netController.sendMsg(parts, ThreePhaseCommitUtility.serializeMessage(commitMessage));
-					}
+					}					
 				}				
 			} else {  // current process is a participant.
 				List<String> receivedMsgs = new ArrayList<String>();
